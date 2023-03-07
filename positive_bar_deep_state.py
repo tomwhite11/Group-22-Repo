a1327c22b09690777c244a7b9a434b90a65c826a
tomwhite11 <69674993+tomwhite11@users.noreply.github.com>
2023-03-07 14:37:12 +0000

    added code for initial plots

---
+++ positive_bar_deep_state.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_pickle("2022-07_tweet_data_deep_state.pkl")
+x = df['tweet_id']
+y = df['positive_tweet_sentiment']
+
+# Horizontal Bar Plot
+plt.bar(x, y)
+
+plt.gca().axes.get_xaxis().set_visible(False)
+# Show Plot
+plt.show()
